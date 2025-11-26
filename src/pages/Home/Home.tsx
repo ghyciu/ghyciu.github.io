a7e0363645d413f95ee87483eed498e4eb18b967
Gio Ciudadano <gcciudadano@gmail.com>
2025-11-27 02:23:19 +0800

    Add cosplay card and cosplay section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Banner from './components/Banner/Banner';
 import About from './components/About/About';
 import Footer from '../../components/Footer/Footer';
+import Cosplay from './components/Cosplay/Cosplay';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Banner />
       <About />
+      <Cosplay />
       <Footer />
     </div>
   );
