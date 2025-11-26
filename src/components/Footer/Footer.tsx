06c21907f88e498a8858900db0ffb8ed465b2196
Gio Ciudadano <gcciudadano@gmail.com>
2025-11-26 22:49:48 +0800

    Add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-left">
+        <p>
+          <i>
+            <b>Ad astra per aspera.</b> Through hardships through the stars.
+          </i>
+        </p>
+        <br />
+        <a href="https://ghyciu.com">ghyciu.com</a>
+        <p>© 2025 Ghyciu. All rights reserved.</p>
+      </div>
+      <div className="footer-right">example</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from './components/Banner/Banner';
 import About from './components/About/About';
+import Footer from '../../components/Footer/Footer';
 
 const Home: React.FC = () => {
   return (
     <div>
       <Banner />
       <About />
+      <Footer />
     </div>
   );
 };
