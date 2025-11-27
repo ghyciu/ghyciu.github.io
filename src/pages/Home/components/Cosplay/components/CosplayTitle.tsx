d2769ef642eaf66e42b85d90a234c118b83c9c05
Gio Ciudadano <gcciudadano@gmail.com>
2025-11-28 01:49:17 +0800

    Add animations to cosplay section

--- src/pages/Home/components/Cosplay/components/CosplayTitle.tsx
+++ src/pages/Home/components/Cosplay/components/CosplayTitle.tsx
@@ -1,5 +1,33 @@
+import React, { useEffect, useRef, useState } from 'react';
+
 const CosplayTitle = () => {
-  return <div className="home-cosplay-title">Cosplay</div>;
+  const [visible, setVisible] = useState(false);
+  const titleRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(
+      entries => {
+        const entry = entries[0];
+        if (entry && entry.isIntersecting) {
+          setVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 }
+    );
+    if (titleRef.current) {
+      observer.observe(titleRef.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div ref={titleRef} className={`home-cosplay-title${visible ? ' float-in-up' : ''}`}>
+      Cosplay
+    </div>
+  );
 };
 
 export default CosplayTitle;
