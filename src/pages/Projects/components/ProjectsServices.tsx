2c03fbe9bfee91da4a570cfd2f6d45fe20c11fae
Gio Ciudadano <gcciudadano@gmail.com>
2025-12-03 02:47:38 +0800

    Add ProjectsServicesCards

--- src/pages/Projects/components/ProjectsServices.tsx
+++ src/pages/Projects/components/ProjectsServices.tsx
@@ -1,22 +1,15 @@
 import Title from '../../../components/Title/Title';
 import Description from '../../../components/Description/Description';
-import { ServicesData, ServicesDataElement } from '../../../data/services';
-import ProjectServicesCard from './ProjectsServicesCard';
+import ProjectsServicesCards from './ProjectsServicesCards';
 
 const ProjectsServices = () => {
-  const services: ServicesDataElement[] = ServicesData;
-
   return (
     <div className="projects-services">
       <Title>Services</Title>
       <Description>
         <p>I'm always looking for new opportunities! If you are hiring or interested in collaborating, feel free to reach out.</p>
       </Description>
-      <div className="projects-services-cards">
-        {services.map((service: ServicesDataElement, idx: number) => (
-          <ProjectServicesCard key={idx} title={service.title} frameworks={service.frameworks} icon_classname={service.icon_classname} />
-        ))}
-      </div>
+      <ProjectsServicesCards />
       <hr />
     </div>
   );
---
+++ src/pages/Projects/components/ProjectsServicesCards.tsx
@@ -0,0 +1,38 @@
+import { ServicesData, ServicesDataElement } from '../../../data/services';
+import ProjectServicesCard from './ProjectsServicesCard';
+import React, { useEffect, useRef, useState } from 'react';
+
+const ProjectsServicesCards = () => {
+  const services: ServicesDataElement[] = ServicesData;
+  const [visible, setVisible] = useState(false);
+  const cardsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(
+      entries => {
+        const entry = entries[0];
+        if (entry && entry.isIntersecting) {
+          setVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 }
+    );
+    if (cardsRef.current) {
+      observer.observe(cardsRef.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className={`projects-services-cards ${visible ? ' float-in-up' : ''}`} ref={cardsRef}>
+      {services.map((service: ServicesDataElement, idx: number) => (
+        <ProjectServicesCard key={idx} title={service.title} frameworks={service.frameworks} icon_classname={service.icon_classname} />
+      ))}
+    </div>
+  );
+};
+
+export default ProjectsServicesCards;
