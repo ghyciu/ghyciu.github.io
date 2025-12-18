81c7de7afa271008448b80376c02500ee427bc4c
Gio Ciudadano <gcciudadano@gmail.com>
2025-12-18 13:49:25 +0800

    Update placeholder image

--- src/data/design.tsx
+++ src/data/design.tsx
@@ -1,6 +1,4 @@
 import DiwataPortfolioImage from '../assets/images/design/diwata-portfolio.png';
-import PlaceholderImage2 from '../assets/images/design/placeholder2.png';
-import PlaceholderImage3 from '../assets/images/design/placeholder3.png';
 
 export interface DesignDataElement {
   image: string;
@@ -11,17 +9,9 @@ export interface DesignDataElement {
 const DesignData: DesignDataElement[] = [
   {
     image: DiwataPortfolioImage,
-    title: 'UPV Diwata Esports',
+    title: 'UPV Diwata Esports (2023)',
     description:
       "Worked as a social media manager and graphic designer for the university's official esports organization. Created various digital artworks, including social media posts, event posters, and promotional materials to enhance the organization's online presence. The UP logo and wordmark used in the designs are official trademarks of the University of the Philippines and are used with explicit permission from the Office of Student Affairs."
-  },
-  {
-    image: PlaceholderImage2,
-    title: 'Placeholder 2'
-  },
-  {
-    image: PlaceholderImage3,
-    title: 'Placeholder 3'
   }
 ];
 
